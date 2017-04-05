663b7d5f7ef24bd425159723bbd3a250e56cd3a3
Tran Thuong Thien <Tran Thuong Thien>
2017-04-05 17:09:59 +0700

    update

--- UmbracoTut/PRSiteUmbraco/Controllers/SiteLayoutController.cs
+++ UmbracoTut/PRSiteUmbraco/Controllers/SiteLayoutController.cs
@@ -4,14 +4,16 @@ namespace PRSiteUmbraco.Controllers
 {
     public class SiteLayoutController : BaseController
     {
+        private const string PARTIAL_VIEW_FOLDER = "~/Views/Partials/SiteLayout/";
+
         public ActionResult RenderHeader()
         {
-            return PartialView("~/Views/Partials/SiteLayout/Header.cshtml");
+            return PartialView(PARTIAL_VIEW_FOLDER + "_Header.cshtml");
         }
 
         public ActionResult RenderFooter()
         {
-            return PartialView("~/Views/Partials/SiteLayout/Footer.cshtml");
+            return PartialView(PARTIAL_VIEW_FOLDER + "_Footer.cshtml");
         }
     }
 }
