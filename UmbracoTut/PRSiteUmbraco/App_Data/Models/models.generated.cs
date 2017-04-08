72d5bed89ee53b3e70721621993b84d856c4b7af
Tran Thuong Thien <tranthiencdsp@gmail.com>
2017-04-08 09:43:29 +0700

    section 7

--- UmbracoTut/PRSiteUmbraco/App_Data/Models/models.generated.cs
+++ UmbracoTut/PRSiteUmbraco/App_Data/Models/models.generated.cs
@@ -20,7 +20,7 @@ using Umbraco.ModelsBuilder.Umbraco;
 
 [assembly: PureLiveAssembly]
 [assembly:ModelsBuilderAssembly(PureLive = true, SourceHash = "cc565b927252a365")]
-[assembly:System.Reflection.AssemblyVersion("0.0.0.2")]
+[assembly:System.Reflection.AssemblyVersion("0.0.0.4")]
 
 namespace Umbraco.Web.PublishedContentModels
 {
--- UmbracoTut/PRSiteUmbraco/Models/HomeModel.cs
+++ UmbracoTut/PRSiteUmbraco/Models/HomeModel.cs
@@ -1,31 +1,56 @@
-﻿using System;
+﻿using Archetype.Models;
+using PRSiteUmbraco.Infrastructure;
+using System;
 using System.Collections.Generic;
 using System.Globalization;
 using Umbraco.Core.Models;
-using Umbraco.Core.Media;
-using PRSiteUmbraco.Infrastructure;
-using Archetype.Models;
 using Umbraco.Web;
-using System.Linq;
-using Umbraco.Core.Services;
-using umbraco;
 
 namespace PRSiteUmbraco.Models
 {
     public class HomeModel : BaseModel
     {
+        private static IPublishedContent PublishedContent;
+
         public HomeModel(IPublishedContent content, CultureInfo culture)
             : base(content, culture)
         {
+            PublishedContent = content;
         }
 
         public HomeModel(IPublishedContent content)
             : base(content)
         {
-            var homePage = content.Homepage();
+            PublishedContent = content;
+            //var homePage = content.Homepage();
+            //var featuredItems = homePage.GetPropertyValue<ArchetypeModel>(Contants.FEATURED_ITEMS);
+
+            //var _featuredItems = new List<FeaturedItem>();
+            //var umbracoHelper = new UmbracoHelper(UmbracoContext.Current);
+            //foreach (var item in featuredItems.Fieldsets)
+            //{
+            //    var imageId = item.GetValue<int>(Contants.Archetype.ALIAS_IMAGE);
+            //    var mediaItem = umbracoHelper.Media(imageId);
+            //    string imageUrl = mediaItem.Url;
+
+            //    int pageId = item.GetValue<int>(Contants.Archetype.ALIAS_PAGE);
+            //    var linkToPage = umbracoHelper.TypedContent(pageId);
+            //    string linkUrl = linkToPage.Url;
+
+            //    _featuredItems.Add(new FeaturedItem(item.GetValue<string>(Contants.Archetype.ALIAS_NAME),
+            //        item.GetValue<string>(Contants.Archetype.ALIAS_CATEGORY),
+            //        imageUrl, linkUrl));
+
+            //    this._featuredItems = _featuredItems;
+            //}
+        }
+
+        Func<IEnumerable<FeaturedItem>> GetFeaturedItems = delegate ()
+        {
+            var homePage = PublishedContent.Homepage();
             var featuredItems = homePage.GetPropertyValue<ArchetypeModel>(Contants.FEATURED_ITEMS);
 
-            _featuredItems = new List<FeaturedItem>();
+            var _featuredItems = new List<FeaturedItem>();
             var umbracoHelper = new UmbracoHelper(UmbracoContext.Current);
             foreach (var item in featuredItems.Fieldsets)
             {
@@ -40,15 +65,21 @@ namespace PRSiteUmbraco.Models
                 _featuredItems.Add(new FeaturedItem(item.GetValue<string>(Contants.Archetype.ALIAS_NAME),
                     item.GetValue<string>(Contants.Archetype.ALIAS_CATEGORY),
                     imageUrl, linkUrl));
-            }
-        }
 
-        private List<FeaturedItem> _featuredItems;
-        public Lazy<List<FeaturedItem>> FeaturedItem
+            }
+            return _featuredItems;
+        };
+
+        //private IEnumerable<FeaturedItem> _featuredItems;
+        public Lazy<IEnumerable<FeaturedItem>> FeaturedItem
         {
             get
             {
-                return new Lazy<List<FeaturedItem>>(_featuredItems);
+                return new Lazy<IEnumerable<FeaturedItem>>(() =>
+                {
+                    return Helper.GetObjectFromCache<IEnumerable<FeaturedItem>>(
+                       string.Format("{0}_featureditems", CurrentCulture.Name), Contants.CACHE_TIME, GetFeaturedItems);
+                });
             }
         }
     }
